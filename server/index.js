210d98a5cf086183b383baaaa4da8f1f0c7c872f
cjsolis <cjelizondo12@gmail.com>
2023-09-04 19:35:55 -0500

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('../schema/schema')
 const port = process.env.PORT || 5000;
+const connectDB = require('./config/db')
 
 const app = express();
 
+// Connect to MongoDB
+connectDB()
 app.use(
     '/graphql',
     graphqlHTTP({
